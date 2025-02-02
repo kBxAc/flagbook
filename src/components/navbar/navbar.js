20afa3e5fe8ac653448009efcd8a382a52918986
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2025-02-02 18:12:26 +0000

    ✨ Add responsive navbar with mobile menu toggle and styles

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,18 +1,66 @@
+"use client";
+
 import "./navbar.css";
 
 import Link from "next/link";
+import { FaBurger } from "react-icons/fa6";
+import { useState } from "react";
 
 export default function Navbar() {
+
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <div className="bg-[--navbar-outer-bg]">
-            <div className="navbar backdrop-blur-lg m-2 p-2 border-2 rounded-lg border-[--navbar-border] bg-[--navbar-inner-bg] text-[--navbar-text] space-x-5">
-                <Link href="/" className="main-link">flagbook</Link>
-                <Link href="/home" className="navbar-link">home</Link>
-                <Link href="/ctf" className="navbar-link">ctfs</Link>
-                <Link href="/writeups" className="navbar-link">writeups</Link>
-                <Link href="/tools" className="navbar-link">tools</Link>
-                <Link href="/about" className="navbar-link">about</Link>
+            <div className="navbar backdrop-blur-lg m-2 p-2 border-2 rounded-lg border-[--navbar-border] bg-[--navbar-inner-bg] text-[--navbar-text]">
+                <Desktop />
+                <Mobile onClick={toggleMenu} />
+                <MobileMenu isOpen={isOpen} />
             </div>
         </div>
     )
+}
+
+function Desktop() {
+    return (
+        <div className="navbar-desktop space-x-5">
+            <div className="main-link">
+                <Link href="/">flagbook</Link>
+            </div>
+            <Link href="/home" className="navbar-link">home</Link>
+            <Link href="/ctf" className="navbar-link">ctfs</Link>
+            <Link href="/writeups" className="navbar-link">writeups</Link>
+            <Link href="/tools" className="navbar-link">tools</Link>
+            <Link href="/about" className="navbar-link">about</Link>
+        </div>
+    )
+}
+
+function Mobile({onClick}) {
+    return (
+        <div className="navbar-mobile">
+            <div className="main-link">
+                <Link href="/">flagbook</Link>
+            </div>
+            <div className="navbar-mobile-button" onClick={onClick}>
+                <FaBurger />
+            </div>
+        </div>
+    )
+}
+
+function MobileMenu({ isOpen }) {
+    return (
+        <div className={`navbar-mobile-menu ${isOpen ? "open" : ""}`}>
+            <Link href="/home" className="navbar-link">home</Link>
+            <Link href="/ctf" className="navbar-link">ctfs</Link>
+            <Link href="/writeups" className="navbar-link">writeups</Link>
+            <Link href="/tools" className="navbar-link">tools</Link>
+            <Link href="/about" className="navbar-link">about</Link>
+        </div>
+    );
 }
